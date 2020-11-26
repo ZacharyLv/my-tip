2bc212514e833a6fc41388ce1b1e1128f25de73a
ny <ny@nydeMacBook-Pro.local>
2020-11-26 21:29:18 +0800

    更新展示方式

--- src/components/learnWord/index.js
+++ src/components/learnWord/index.js
@@ -5,14 +5,15 @@ let index = 0; // 需要弹出来的单词的索引
 
 const run = () => {
   const wordList = JSON.parse(Store.get('word'));
-  const [word, translate] = wordList[index];
+
+  if (wordList[index]) {
+    Notification({
+      title: '暖洋想去放风筝',
+      body: wordList[index]
+    });
+  }
 
   index = index >= wordList.length - 1 ? 0 : index + 1;
-
-  Notification({
-    title: '暖洋想去放风筝',
-    body: `${word}: ${translate}`
-  });
 
   setTimeout(() => {
     run();
